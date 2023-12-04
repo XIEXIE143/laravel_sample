efc3491a95b1d5dd8f364e59287360cce671b42e
XIEXIE143 <xjy143@outlook.com>
2023-12-04 10:04:25 +0900

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ItemRequest;
 use Illuminate\Http\Request;
 use App\Models\Item;
 
@@ -9,9 +10,6 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //SELECT * FROM items;
@@ -21,21 +19,16 @@ class ItemController extends Controller
         return view('item.index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('item.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(ItemRequest $request)
     {
         // リクエストから POSTデータを取得
         $posts = $request->input();
+
         // INSERT INTO items (name, price) VALUES (xxxx, xxxx);
         Item::create($posts);
 
@@ -43,9 +36,6 @@ class ItemController extends Controller
         return redirect(route('item.index'));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(int $id)
     {
         //TODO: MySQLデータベースから取得
@@ -68,6 +58,7 @@ class ItemController extends Controller
      */
     public function edit(string $id)
     {
+        // SELECT * FROM items WHERE id = xx;
         $item = Item::find($id);
         if (!$item) return redirect(route('item.index'));
         $data = ['item' => $item];
@@ -77,15 +68,16 @@ class ItemController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ItemRequest $request, string $id)
     {
         // dd($id);
         $data = $request->all();
         //Tokenを削除
         // unset($data['_token']);
-        // UPDATE items SET xxx = xxx, ... WHERE id = xx;
         // Item::where('id', $id)->update($data);
-        
+
+        // SELECT * FROM items WHERE id = xx;
+        // UPDATE items SET xxx = xxx, ... WHERE id = xx;
         Item::find($id)->fill($data)->save();
 
         //編集画面にリダイレクト
