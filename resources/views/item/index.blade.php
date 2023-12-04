835bbfac348cc6a4e3383af3ab95dce38876b8d5
XIEXIE143 <xjy143@outlook.com>
2023-12-04 13:51:56 +0900

    update

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -4,6 +4,14 @@
         <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white">Item List</h2>
 
+            <div class="mb-3">
+                <form action="{{ route('item.index') }}" method="get">
+                    <input type="text" name="item_name" value="{{ $item_name }}" class="border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                    <button class="bg-blue-500 text-sm text-white py-1 px-4 rounded focus:outline-none focus:shadow-outline">{{__('message.search')}}</button>
+                    <a href="{{ route('item.index') }}" class="bg-white text-blue-500 font-bold py-2 px-4 rounded">{{__('message.clear')}}</a>
+                </form>
+            </div>
+
             <a href="{{ route('item.create') }}" class="bg-white text-blue-500 font-bold py-2 px-4 rounded">New</a>
             <table class="text-sm text-left rtl:text-right text-gray-500">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -14,6 +22,20 @@
                     </th>
                     <th scope="col" class="px-6 py-3">
                         {{ __('Price') }}
+                        <a href="{{ 
+                            route('item.index', 
+                            [ 
+                                'order_column' => 'price',
+                                'order_value' => 'desc'
+                            ])
+                        }}">▼</a>
+                        <a href="{{ 
+                            route('item.index', 
+                            [ 
+                                'order_column' => 'price',
+                                'order_value' => 'asc'
+                            ])
+                        }}">▲</a>
                     </th>
                 </thead>
                 <tbody>
